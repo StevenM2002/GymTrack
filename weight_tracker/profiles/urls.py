f0e1989e0523a3c941410cc1254bc64129728cca
Steven <stevenmai1111@hotmail.com>
2023-02-01 16:39:37 +1100

    moved backend to diff repo

--- weight_tracker/profiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import SignUpAPI, Signer
-from rest_framework.authtoken.views import obtain_auth_token
-
-urlpatterns = [
-    path("signup/", SignUpAPI.as_view()),
-    path("login/", obtain_auth_token),
-    path("check/", Signer.as_view()),
-]
--- weight_tracker/tracking/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("session/", SessionTrack.as_view()),
-    path("exercise/", WeightExerciseTrack.as_view()),
-    path("setrep/", SetRepInfoTrack.as_view()),
-]
