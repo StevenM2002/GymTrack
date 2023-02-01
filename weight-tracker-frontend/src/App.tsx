c1a38ba74cec55263908627918b61a8b709bdcda
Steven <stevenmai1111@hotmail.com>
2023-02-01 17:34:50 +1100

    trying to deploy

--- weight-tracker-frontend/src/App.tsx
+++ weight-tracker-frontend/src/App.tsx
@@ -18,7 +18,7 @@ export function getCookie(key: string) {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/GymTrack' element={<LoginPage />} />
